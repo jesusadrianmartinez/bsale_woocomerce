0d733654da2efd46a2367a6e80582d86576b3495
jesusadrianmartinez <jesus.a.martinez.l@gmail.com>
2021-09-07 20:41:54 -0300

    Iniciando
    Pluggins inicial

---
+++ bsale_woocomerce.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: BSale Woocommerce Integration
+ * Description: Plugin para la integración BSALE con Woocommerce. Si tienes Bsale y tu tienda con woocommerce esta herramienta te enlaza el Bsale para que puedas tener tu stock en tiempo real.
+ * Version: 1.0.0
+ * Author: Neurona Digital
+ * Author URI: https://neuronadigital.cl/
+ * Developer Chief: Jose Hernandez
+ * Developer Assitan: Roberto Flores
+ * Developer Assitan: Jesus Martinez
+ * Developer URI: https://neuronadigital.cl/
+ * Text Domain: bsale-woocommerce-integ
+ * Domain Path: 
+ * languages: Spanish
+ *
+ * WC requires at least: 2.2
+ * WC tested up to: 3.5
+ *
+ * Copyright: © 2017-2021 Neurona Digital SpA.
+ * License: GNU General Public License
